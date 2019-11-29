9043f98d443637878c18cfd6300367ca4d4ceeb1
Anton Chernov2 <anton_chernov2@epuavinw0183.kyiv.epam.com>
2019-11-29 15:57:44 +0200

    start work on handlers

--- main.go
+++ main.go
@@ -20,25 +20,36 @@ type JSONData struct {
 	viewrname string
 }
 
-var db *sql.DB
+// var db *sql.DB
+
+// Env global structure for save all neaded global variables
+type Env struct {
+	db *sql.DB
+}
 
 // InitDB initiate connection to database
-func InitDB(dataSourceName string) {
-	var err error
-	db, err = sql.Open("postgres", dataSourceName)
+func InitDB(dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
-		log.Panic(err)
+		return nil, err
 	}
-
 	if err = db.Ping(); err != nil {
-		log.Panic(err)
+		return nil, err
 	}
+	return db, nil
 }
 
-// FCMTokenSaver handler for saving FCM tokens in database
-func FCMTokenSaver(w http.ResponseWriter, r *http.Request) {
+// 	var err error
+// 	// env = &Env{}
+// 	db, err = sql.Open("postgres", dataSourceName)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
 
-}
+// 	if err = db.Ping(); err != nil {
+// 		log.Panic(err)
+// 	}
+// }
 
 func main() {
 	var hostPort string = "127.0.0.1:8800"
@@ -50,7 +61,13 @@ func main() {
 		databaseURI = dbURI
 	}
 
-	defer InitDB(databaseURI)
+	db, err := InitDB(databaseURI)
+
+	if err != nil {
+		log.Panicf("Database connection error! %v", err)
+	}
+
+	env := &Env{db: db}
 
 	hpGlVar := os.Getenv("SERVER")
 
@@ -59,7 +76,22 @@ func main() {
 	}
 
 	log.Printf("Servers started at: %v", hostPort)
-
+	http.Handle("/", CreateFCMToken(env))
 	fmt.Println(http.ListenAndServe(hostPort, nil))
+
+}
+
+// CreateFCMToken handler for saving FCM tokens in database
+func CreateFCMToken(w http.ResponseWriter, r *http.Request, env *Env) {
+	// TODO finish   https://www.alexedwards.net/blog/organising-database-access
+}
+
+// UpdateFCMToken handler for saving FCM tokens in database
+func UpdateFCMToken(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeleteFCMToken handler for saving FCM tokens in database
+func DeleteFCMToken(w http.ResponseWriter, r *http.Request) {
 
 }
